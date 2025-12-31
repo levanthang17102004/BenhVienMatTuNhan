8bff2b312d863c02ef724d76f0ec42fce67f4daf
levanthang17102004 <levanthang17102004@gmail.com>
2025-12-31 08:47:11 +0700

    cap nhat contact

--- back-end/src/models/contact.model.js
+++ back-end/src/models/contact.model.js
@@ -5,45 +5,21 @@ const contactSchema = new mongoose.Schema(
         user_id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
-            default: null, // khách vãng lai
-            index: true,
+            default: null,
         },
 
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-
-        email: {
-            type: String,
-            required: true,
-            lowercase: true,
-            trim: true,
-            match: [/^\S+@\S+\.\S+$/, "Email không hợp lệ"],
-        },
-
-        phoneNumber: {
-            type: String,
-            trim: true,
-        },
-
-        message: {
-            type: String,
-            required: true,
-        },
+        name: String,
+        email: String,
+        phoneNumber: String,
+        message: String,
 
         status: {
             type: String,
             enum: ["NEW", "IN_PROGRESS", "RESOLVED"],
             default: "NEW",
-            index: true,
         },
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    { timestamps: true, versionKey: false }
 );
 
 module.exports = mongoose.model("Contact", contactSchema);
